d08ad73dc02de66ee70d6d5a0c5849f2946c379d
barannapolci <apuhlij66@gmail.com>
2026-07-27 22:05:27 +0300

    feat: configure EF Core entity relationships

---
+++ LeadBridge.Core.Infrastructure.Postgres/Configurations/EmailConfirmationConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using LeadBridge.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LeadBridge.Core.Infrastructure.Postgres.Configurations
+{
+    public class EmailConfirmationConfiguration : IEntityTypeConfiguration<EmailConfirmation>
+    {
+        public void Configure(EntityTypeBuilder<EmailConfirmation> builder)
+        {
+            ArgumentNullException.ThrowIfNull(builder);
+
+            builder.HasKey(e => e.Id);
+
+            builder.HasOne(e => e.User)
+                .WithOne()
+                .HasForeignKey<EmailConfirmation>(e => e.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
--- LeadBridge.Core.Infrastructure.Postgres/Configurations/ReviewsConfiguration.cs
+++ LeadBridge.Core.Infrastructure.Postgres/Configurations/ReviewsConfiguration.cs
@@ -8,6 +8,20 @@ public class ReviewsConfiguration : IEntityTypeConfiguration<Reviews>
 {
     public void Configure(EntityTypeBuilder<Reviews> builder)
     {
-        throw new NotImplementedException();
+        ArgumentNullException.ThrowIfNull(builder);
+
+        builder.HasKey(r => r.Id);
+
+        builder.HasOne(r => r.Sender)
+            .WithMany(u => u.GivenReviews)
+            .HasForeignKey(r => r.SenderId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(r => r.Recipient)
+            .WithMany(u => u.RecivedReviews)
+            .HasForeignKey(r => r.RecipientId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasIndex(r => new { r.SenderId, r.RecipientId }).IsUnique();
     }
 }
